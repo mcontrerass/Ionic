dd81d506432641fa4ca1e134b4b7c86da8613bea
mcontrerass <mcontrerass@gmail.com>
2019-03-23 22:50:54 -0300

    Prueba

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'portfolio', loadChildren: './portfolio/portfolio.module#PortfolioPageModule' },
+  { path: 'user-detail', loadChildren: './user-detail/user-detail.module#UserDetailPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
